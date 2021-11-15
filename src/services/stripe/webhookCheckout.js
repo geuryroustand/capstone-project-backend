663635cfdfc2b0cbcb884d15119bb3ddc5f52a74
Geury <theromantic2010@hotmail.com>
2021-11-15 12:43:31 +0100

    Update webhookCheckout.js

--- src/services/stripe/webhookCheckout.js
+++ src/services/stripe/webhookCheckout.js
@@ -14,62 +14,61 @@ webHooCheckoutRouter.post("/", async (req, res, next) => {
       signature,
       process.env.STRIPE_WEBHOOKS_SECRET
     );
+    if (event.type === "checkout.session.completed") {
+      const { customer_email, amount_total } = event.data.object;
+      const {
+        name,
+        surname,
+        phoneNumber,
+        arrivalAirlineName,
+        arrivalFlightNumber,
+        arrivalDepartureAirport,
+        arrivalDate,
+
+        departureAirlineName,
+        departureFlightNumber,
+        departureDepartureAirport,
+        departureDate,
+
+        journey,
+        passengers,
+        taxiOption,
+      } = event.data.object.metadata;
+
+      const createBooking = await bookingSchema.create({
+        phoneNumber,
+        arrivalAirlineName,
+        arrivalFlightNumber,
+        arrivalDepartureAirport,
+        arrivalDate,
+
+        departureAirlineName,
+        departureFlightNumber,
+        departureDepartureAirport,
+        departureDate,
+
+        journey,
+        passengers,
+        taxiOption,
+
+        totalPrice: amount_total / 100,
+        email: customer_email,
+        passengersName: name,
+        passengersSurname: surname,
+      });
+
+      console.log("======amount", amount_total / 100);
+      // console.log("=================ddd", event.data.object);
+      console.log(
+        "=========dddd===========",
+        event.data.object
+        // event.data.object.customer_email
+      );
+
+      res.status(200).send({ received: true, createBooking });
+    }
   } catch (error) {
     return res.status(400).send(`Webhook error: ${error}`);
-  }
-
-  if (event.type === "checkout.session.completed") {
-    const { customer_email, amount_total } = event.data.object;
-    const {
-      name,
-      surname,
-      phoneNumber,
-      arrivalAirlineName,
-      arrivalFlightNumber,
-      arrivalDepartureAirport,
-      arrivalDate,
-
-      departureAirlineName,
-      departureFlightNumber,
-      departureDepartureAirport,
-      departureDate,
-
-      journey,
-      passengers,
-      taxiOption,
-    } = event.data.object.metadata;
-
-    const createBooking = await bookingSchema.create({
-      phoneNumber,
-      arrivalAirlineName,
-      arrivalFlightNumber,
-      arrivalDepartureAirport,
-      arrivalDate,
-
-      departureAirlineName,
-      departureFlightNumber,
-      departureDepartureAirport,
-      departureDate,
-
-      journey,
-      passengers,
-      taxiOption,
-
-      totalPrice: amount_total / 100,
-      email: customer_email,
-      passengersName: name,
-      passengersSurname: surname,
-    });
-
-    console.log("======amount", amount_total / 100);
-    // console.log("=================ddd", event.data.object);
-    console.log(
-      "=========dddd===========",
-      event.data.object
-      // event.data.object.customer_email
-    );
-
-    res.status(200).send({ received: true, createBooking });
   }
 });
 
