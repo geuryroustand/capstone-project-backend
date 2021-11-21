b9e81d118a281d4c46f46a57c02fd356e7348151
Geury <theromantic2010@hotmail.com>
2021-11-21 16:33:28 +0100

    Update userSchema.js

--- src/services/user/userSchema.js
+++ src/services/user/userSchema.js
@@ -3,28 +3,33 @@ import bcrypt from "bcrypt";
 
 const { model, Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "User name is required"],
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+    },
+    surname: {
+      type: String,
+      required: [true, " Surname is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, " Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  surname: {
-    type: String,
-    required: [true, " Surname is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, " Password is required"],
-  },
-  refreshToken: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
